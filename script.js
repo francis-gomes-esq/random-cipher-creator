42ced914fb1f8114093ebb31de3307bdd0905205
francis-gomes-esq <figomesokoye@protonmail.com>
2024-01-07 21:56:57 +0000

    add logic for getting a random element from an array

--- script.js
+++ script.js
@@ -112,7 +112,10 @@ function getPasswordOptions() {
 // getPasswordOptions()
 
 // Function for getting a random element from an array
-function getRandom(arr) {}
+function getRandom(arr) {
+	var randomIndex = Math.floor(Math.random() * arr.length)
+	return arr[randomIndex]
+}
 
 // Function to generate password with user input
 function generatePassword() {
